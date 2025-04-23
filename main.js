71b7904755d3b1aa70f89de8659ab71246b576a0
matheus henrique pereira gangini <mh830836@gmail.com>
2025-04-22 21:00:17 -0300

    java quase finalizado porém consta alguns erros que terei que localizar

--- main.js
+++ main.js
@@ -18,3 +18,30 @@ form.addEventListener('submit', function (e) {
     form.reset();
   }
 });
+function atualizarTabela() {
+    tabela.innerHTML = '';
+    let total = 0;
+  
+    gastos.forEach((gasto, index) => {
+      const tr = document.createElement('tr');
+  
+      const classeValor = gasto.valor > 100 ? 'valor-alto' : '';
+  
+      tr.innerHTML = `
+        <td>${gasto.descricao}</td>
+        <td class="${classeValor}">R$ ${gasto.valor.toFixed(2)}</td>
+        <td>${gasto.categoria}</td>
+        <td><button class="remover" onclick="removerGasto(${index})">Excluir</button></td>
+      `;
+  
+      tabela.appendChild(tr);
+      total += gasto.valor;
+    });
+  
+    totalSpan.textContent = total.toFixed(2);
+  }
+  
+  function removerGasto(index) {
+    gastos.splice(index, 1);
+    atualizarTabela();
+  }
